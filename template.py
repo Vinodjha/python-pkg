da7b7cda22ba2f00bec95c2033588a842906e2f1
vinodjha <v.jha85@gmail.com>
2025-06-25 11:08:54 +0200

    initial commit

---
+++ template.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path 
+
+package_name = "mongodb_connector"
+
+list_of_files = [
+   ".github/workflows/ci.yaml",
+   "src/__init__.py",
+   f"src/db-connector/__init__.py", 
+   f"src/db-connector/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/unit/unit.py",
+   "tests/integration/__init__.py",
+   "tests/integration/integration.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements-dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb", 
+]
+
+for file in list_of_files:
+    file_path = Path(file)
+    file_dir, file_name = os.path.split(file_path)
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+    if ((not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0)):
+        with open(file_path, "w+") as f:
+            pass
